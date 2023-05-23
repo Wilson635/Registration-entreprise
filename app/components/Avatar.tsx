4e8f0c25131f65b40bd5f0f39dba7bff31354938
Wilson635 <87822542+Wilson635@users.noreply.github.com>
2023-05-23 14:17:57 +0100

    updates files

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -9,14 +9,14 @@ interface AvatarProps {
 
 const Avatar: React.FC<AvatarProps> = ({ src }) => {
   return ( 
-    // <Image 
-    //   className="rounded-full" 
-    //   height="30" 
-    //   width="30" 
-    //   alt="Avatar" 
-    //   src={src || '/images/placeholder.jpg'}
-    // />
-    <MdAccountCircle className="text-4xl text-gray-500"/>
+    <Image 
+      className="rounded-full" 
+      height="30" 
+      width="30" 
+      alt="Avatar" 
+      src={src || '/images/placeholder.jpg'}
+    />
+    // <MdAccountCircle className="text-4xl text-gray-500"/>
    );
 }
  
