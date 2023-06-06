23cb050d386e8cda8f4f4ea1b17d2c1efff2008e
Wilson635 <87822542+Wilson635@users.noreply.github.com>
2023-06-06 15:40:34 +0100

    Update page.tsx

--- app/entreprise/page.tsx
+++ app/entreprise/page.tsx
@@ -1,87 +1,81 @@
 
 
-// import EntrepriseHead from "../components/entreprise/EntrepriseHead";
-// import EmptyState from "@/app/components/EmptyState";
-// import ClientOnly from "@/app/components/ClientOnly";
+import EntrepriseHead from "../components/entreprise/EntrepriseHead";
+import EmptyState from "@/app/components/EmptyState";
+import ClientOnly from "@/app/components/ClientOnly";
 
-// import getCurrentUser from "@/app/actions/getCurrentUser";
-// import getListings, {
-//     IListingsParams
-// } from "@/app/actions/getListings";
-// import Container from "@/app/components/Container";
-// import { FaSlackHash } from "react-icons/fa";
-// import Heading from "../components/Heading";
-// import NewLetter from "../components/Newletter";
-// import ListingCard from "../components/listings/ListingCard";
-
-
-// interface HomeProps {
-//     searchParams: IListingsParams
-// };
-
-// const Entreprise = async ({ searchParams }: HomeProps) => {
-
-//     const currentUser = await getCurrentUser();
-//     const listings = await getListings( searchParams );
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import getListings, {
+    IListingsParams
+} from "@/app/actions/getListings";
+import Container from "@/app/components/Container";
+import { FaSlackHash } from "react-icons/fa";
+import Heading from "../components/Heading";
+import NewLetter from "../components/Newletter";
+import ListingCard from "../components/listings/ListingCard";
 
 
-//     if (listings.length === 0) {
-//         return (
-//             <ClientOnly>
-//                 <EmptyState showReset />
-//             </ClientOnly>
-//         );
-//     }
+interface HomeProps {
+    searchParams: IListingsParams
+};
 
-//     return (
-//         <>
-//             <EntrepriseHead />
-//             <ClientOnly>
-//                 <Container>
-//                     <div className="flex gap-3 mt-28">
-//                         <FaSlackHash className="text-indigo-500 text-3xl" />
-//                         <Heading
-//                             title="Entreprises"
-//                             subtitle="Listes des entreprises!"
-//                         />
-//                     </div>
-//                     <div
-//                         className="
-//                             pt-16
-//                             grid 
-//                             grid-cols-1 
-//                             sm:grid-cols-2 
-//                             md:grid-cols-3 
-//                             lg:grid-cols-4
-//                             xl:grid-cols-5
-//                             2xl:grid-cols-6
-//                             gap-8
-//                         "
-//                     >
-//                         {listings.map((listing: any) => (
-//                             <ListingCard
-//                                 currentUser={currentUser}
-//                                 key={listing.id}
-//                                 data={listing}
-//                             />
-//                         ))}
-//                     </div>
-//                 </Container>
-//                 <NewLetter />
-//             </ClientOnly>
-//         </>
-//     );
-// }
+const Entreprise = async ({ searchParams }: HomeProps) => {
 
-// export default Entreprise;
+    const currentUser = await getCurrentUser();
+    const listings = await getListings( searchParams );
 
 
-const page = () => {
+    if (listings.length === 0) {
+        return (
+            <ClientOnly>
+                <EmptyState showReset />
+            </ClientOnly>
+        );
+    }
+
     return (
-        <div>
-            Liste des entreprises
-        </div>
+        <>
+            <EntrepriseHead />
+            <ClientOnly>
+                <Container>
+                    <div className="flex gap-3 mt-28">
+                        <FaSlackHash className="text-indigo-500 text-3xl" />
+                        <Heading
+                            title="Entreprises"
+                            subtitle="Listes des entreprises!"
+                        />
+                    </div>
+                    <div
+                        className="
+                            pt-16
+                            grid 
+                            grid-cols-1 
+                            sm:grid-cols-2 
+                            md:grid-cols-3 
+                            lg:grid-cols-4
+                            xl:grid-cols-5
+                            2xl:grid-cols-6
+                            gap-8
+                        "
+                    >
+                        {listings.map((listing: any) => (
+                            <ListingCard
+                                currentUser={currentUser}
+                                key={listing.id}
+                                data={listing}
+                            />
+                        ))}
+                    </div>
+                </Container>
+                <NewLetter />
+            </ClientOnly>
+        </>
     );
 }
 
-export default page;
+export default Entreprise;
+
+
+export const dynamic = 'force-dynamic';
+// 'auto' | 'force-dynamic' | 'error' | 'force-static'
+
