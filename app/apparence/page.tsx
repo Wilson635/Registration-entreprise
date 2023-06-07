7992df6baf821daef40e13ea2a7c7fd85c63806a
Wilson635 <87822542+Wilson635@users.noreply.github.com>
2023-06-07 14:58:59 +0100

    Create page.tsx

---
+++ app/apparence/page.tsx
@@ -0,0 +1,13 @@
+import ContentSider from "../setting/page";
+
+const page = () => {
+    return (
+        <>
+            <ContentSider>
+                <div>Apparence page</div>
+            </ContentSider>
+        </>
+    );
+}
+
+export default page;
